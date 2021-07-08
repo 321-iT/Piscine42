4233c57c3c8413aa932189cbf18e461bd4de4f1e
Ange SCOTTO DI CARLO <ascotto-@e3r7p4.42.fr>
2021-07-08 18:37:22 +0200

    push c05 corrected

--- C_05/ex07/ft_find_next_prime.c
+++ C_05/ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: ascotto- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 10:10:58 by ascotto-          #+#    #+#             */
-/*   Updated: 2021/07/08 15:23:55 by ascotto-         ###   ########.fr       */
+/*   Updated: 2021/07/08 17:21:33 by ascotto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@ int	ft_is_prime7(int nb)
 	int	i;
 
 	i = 3;
-	if (nb <= 2)
+	if (nb < 2)
+		return (0);
+	if (nb == 2)
 		return (1);
 	if (nb % 2 == 0)
 		return (0);
@@ -33,11 +35,7 @@ int	ft_find_next_prime(int nb)
 	unsigned int	i;
 
 	i = nb;
-	if (nb < 0)
-		return (2);
-	if (nb == 0 || nb == 1)
-		return (2);
-	if (nb == 2)
+	if (nb <= 2)
 		return (2);
 	while (i <= 2147483647)
 	{
